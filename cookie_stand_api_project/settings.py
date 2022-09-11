f0e1a6eeb09a7335871d12b678d0d9de2b3e1467
brianjtarte <brianjtarte@gmail.com>
2022-09-11 08:38:08 -0700

    finally got docker working properly - sheesh

--- cookie_stand_api_project/settings.py
+++ cookie_stand_api_project/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'cookie_stand_api_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
@@ -125,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- cookie_stand_app/urls.py
+++ cookie_stand_app/urls.py
@@ -3,5 +3,5 @@ from .views import CookieList, CookieDetail
 
 urlpatterns = [
     path('', CookieList.as_view(), name='cookie_list'),
-    path('/<int:pk>', CookieDetail.as_view(), name='cookie_detail')
+    path('<int:pk>', CookieDetail.as_view(), name='cookie_detail')
 ]
